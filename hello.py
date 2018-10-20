24029129abfeea2bbf7e16717676a739a2d90ffd
DT2004 <44294436+DT2004@users.noreply.github.com>
2018-10-20 11:43:45 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,2 @@
-print "Hello, World!"
+print "Hello, World!" #this is used for python 2 which is most likely will be of no use in 2020
+print ("Hello, World!") #this is used for python 3 the latest version of python , now all you need to do is run the python intrepeter
