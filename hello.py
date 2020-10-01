87ae4b5a5a25529c45fe1b7746907da88e618e26
prabin123-proo <71684870+prabin123-proo@users.noreply.github.com>
2020-10-01 20:27:16 +0545

    USE YOUR CREATIVITY
    i will suggest to those new programmer of python or any language to make this type of hello world program which is more impactful or motivating than simple hello world. USE YOUR CREATIVITY :)

--- hello.py
+++ hello.py
@@ -1,1 +1,8 @@
-print "Hello, World!"
+print("hh       hh   eeeeeeeeee  l                   l                oooooooooooooo      W          ")
+print("hh       hh   eeeeeeee    ll                 ll                oo          oo      O ")
+print("hh       hh   ee          lll               lll                oo          oo      R")
+print("hh       hh   ee          lll               lll                oo          oo      L") 
+print("hh       hh   ee          lll               lll                oo          oo      D")
+print("hhhhhhhhhhh   eeeeeee     lll               lll                oo          oo      :)
+print("hh       hh   eeeeeee     lllllllllllllll   llllllllllllll      ooooooooooooo                                       ")
+print("hh       hh   eeeeeeeee   lllllllllllll     llllllllllll                                              ")
